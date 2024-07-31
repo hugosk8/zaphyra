22eb1f8e1aebd699c1f9ee877056d1e58c8746ae
Hugo Sauvage <124774769+hugosk8@users.noreply.github.com>
2024-07-31 14:54:32 +0200

    first commit

---
+++ app/Controller/AppController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Core\Controller\Langue;
+use Core\Controller\NavLangue;
+use App\App;
+use Core\Controller\Controller;
+
+class AppController extends Controller
+{
+
+    protected $template = 'default';
+    protected $langue = 'fr';
+    protected $recaptcha = "";
+
+    public function __construct()
+    {
+        $this->viewPath = ROOT . '/app/Views/';
+        $this->template = 'front/template';
+        $this->recaptcha = URL;
+    }
+
+    protected function loadModel($model_name)
+    {
+        $this->$model_name  =  App::getInstance()->getTable($model_name);
+    }
+
+}
